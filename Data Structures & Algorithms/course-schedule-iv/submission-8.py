9fe3cc7799a436a0e8417b28dada3e88e3123096
Pranaya Murugan <87331668+pranayam06@users.noreply.github.com>
2026-07-14 20:58:33 -0400

    Add: course-schedule-iv - submission-8

---
+++ Data Structures & Algorithms/course-schedule-iv/submission-8.py
@@ -0,0 +1,37 @@
+class Solution:
+    def checkIfPrerequisite(self, numCourses: int, prerequisites: List[List[int]], queries: List[List[int]]) -> List[bool]:
+        adjlist = defaultdict(list) 
+        cache = dict()
+
+        for (preq, crs) in prerequisites: 
+            adjlist[crs].append(preq)
+        
+
+        def check(crs): 
+            if not adjlist[crs]: 
+                cache[crs] = set()
+                return cache[crs]
+            if crs in cache:
+                return cache[crs]
+            
+            res = set()
+
+            for preq in adjlist[crs]: 
+                res = res | check(preq)
+                res.add(preq)
+            
+            cache[crs] = res
+            return res
+
+        res_fin = []
+        for u, v in queries: 
+            if v in cache:             
+                res_fin.append(u in cache[v])
+            else:
+                path = check(v)
+                res_fin.append(u in path)
+
+        return res_fin
+
+
+             
