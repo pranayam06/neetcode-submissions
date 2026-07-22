ed2999b7fce39bb19edc36ac150a1ea6ba8206a9
Pranaya Murugan <87331668+pranayam06@users.noreply.github.com>
2026-07-21 20:10:22 -0400

    Add: foreign-dictionary - submission-14

---
+++ Data Structures & Algorithms/foreign-dictionary/submission-14.py
@@ -0,0 +1,52 @@
+class Solution:
+    def foreignDictionary(self, words: List[str]) -> str:
+        
+        # shorter word first 
+        adjlist = defaultdict(list)
+        all_chars = []  
+
+        for word in words:
+            for char in word: 
+                all_chars.append(char)
+
+        for i in range(len(words)-1): 
+            first = words[i] 
+            sec = words[i+1]
+            ptr = 0
+            while ptr < min(len(first), len(sec)):
+                if first[ptr] == sec[ptr]: 
+                    adjlist[first[ptr]]
+                    ptr += 1 
+                    if ptr == len(sec) and ptr < len(first):
+                        return "" 
+                else:
+                    adjlist[sec[ptr]].append(first[ptr])
+                    break
+        print(adjlist)
+        path = set()
+        visited = set()
+        def dfs(c):   
+            if c in visited: 
+                return True 
+            if c in path: 
+                return False  
+            
+            path.add(c)
+            for letter in adjlist[c]:
+                if not dfs(letter):
+                    return False 
+            visited.add(c)
+            res.append(c)
+            return True 
+        
+        res = [] 
+        
+        for char in all_chars:
+            if not dfs(char):
+                return ""
+            path.clear()
+
+        return "".join(res)
+                
+                
+
