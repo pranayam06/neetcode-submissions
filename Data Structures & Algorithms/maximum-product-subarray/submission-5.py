7205bf9e1e8e5ba5e9b21c33308d9331f68d7d22
Pranaya Murugan <87331668+pranayam06@users.noreply.github.com>
2026-08-31 20:42:24 -0400

    Add: maximum-product-subarray - submission-5

---
+++ Data Structures & Algorithms/maximum-product-subarray/submission-5.py
@@ -0,0 +1,25 @@
+class Solution:
+    def maxProduct(self, nums: List[int]) -> int:
+        neg = 10
+        pos = 0
+        res = -float('inf')
+        
+        for num in nums: 
+            pos = max(1,pos)
+            neg = min(1, neg)
+            if num < 0: 
+                pos, neg = neg * num, pos * num 
+            if num > 0: 
+                pos = pos * num 
+                neg = neg * num 
+            if num == 0: 
+                neg = pos = 0  
+
+            res = max(pos, max(res, neg))
+
+            
+        return res
+
+            
+            
+                
