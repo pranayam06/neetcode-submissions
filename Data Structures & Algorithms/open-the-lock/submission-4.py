1704c987de5571ffb74a284876b9961ba458bc56
Pranaya Murugan <87331668+pranayam06@users.noreply.github.com>
2026-09-12 20:22:43 -0400

    Add: open-the-lock - submission-4

---
+++ Data Structures & Algorithms/open-the-lock/submission-4.py
@@ -0,0 +1,38 @@
+class Solution:
+    def openLock(self, deadends: List[str], target: str) -> int:
+        
+        # use bfs because we want minimum 
+        # prune at deadends 
+        # do mod 
+
+        de = set(deadends)
+        seen = set()
+
+        q = deque()
+        q.append("0000")
+        if "0000" in de: 
+            return -1
+        turns = 0 
+        while q: 
+            for _ in range(len(q)): 
+                str_comb = q.popleft()
+                if str_comb == target: 
+                    return turns
+                
+
+                comb = list(map(int, list(str_comb)))
+                for i in range(4): 
+                    plus = ''.join(map(str, comb[:i] + [(comb[i] + 1) % 10] + comb[i+1:]))
+                    if plus not in de and plus not in seen: 
+                        seen.add(plus)
+                        q.append(plus)
+                    minus = ''.join(map(str,comb[:i] + [((comb[i] - 1) % 10)] + comb[i+1:]))
+                    if minus not in de and minus not in seen: 
+                        seen.add(minus)
+                        q.append(minus)
+            turns += 1
+    
+        return -1
+
+
+
