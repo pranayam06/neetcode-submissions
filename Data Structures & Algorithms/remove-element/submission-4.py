031dcbf51dd47a55b93a3612c52745fa1f8e035e
Pranaya Murugan <87331668+pranayam06@users.noreply.github.com>
2026-09-19 12:29:36 -0400

    Add: remove-element - submission-4

---
+++ Data Structures & Algorithms/remove-element/submission-4.py
@@ -0,0 +1,15 @@
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        # swap each element with a num at the end of the list 
+        l = 0 # num we look at 
+        r = len(nums)- 1 # next num we want to swap with 
+
+        while (l <= r): 
+            if nums[l] == val:
+                nums[l] = nums[r]
+                r -= 1  
+            else:
+                l += 1 
+            
+        
+        return l 
