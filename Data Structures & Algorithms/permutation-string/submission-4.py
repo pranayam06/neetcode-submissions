2cd5d69bd59f7702df27222b63abe99e8fd50253
Pranaya Murugan <87331668+pranayam06@users.noreply.github.com>
2026-09-21 11:18:15 -0400

    Add: permutation-string - submission-4

---
+++ Data Structures & Algorithms/permutation-string/submission-4.py
@@ -0,0 +1,24 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        # dont need to rebuild your frequency of each s2 window 
+
+        hmap = Counter(s1)
+        l = r = 0
+        while (l <= r < len(s2)): 
+            while (l < r and hmap[s2[r]] == 0): 
+                hmap[s2[l]] += 1  
+                l+= 1
+                # if still 
+            if hmap[s2[r]] == 0: 
+                l += 1
+                r += 1
+            else: 
+                hmap[s2[r]] -= 1 
+                print(r)
+                print(l)
+                if r-l+1 == len(s1):  
+                    # good str 
+                    return True 
+                r+= 1
+        return False
+                
